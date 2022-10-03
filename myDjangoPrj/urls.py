14b07bdb38c0342dcb34a1bb25d6308b7e24605e
MinseoShindor <tlsalstj01@duksung.ac.kr>
2022-10-04 03:05:34 +0900

    0929 InternetProgramming

--- myDjangoPrj/urls.py
+++ myDjangoPrj/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls), #(IP주소/admin)
     path('blog/', include('blog.urls')), #(IP주소/blog)
